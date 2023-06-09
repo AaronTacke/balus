05e9e38e2c49b06fd58add90bfb58c4cb7c79225
Sebastian Schneider <sebastian_f_schneider@web.de>
2023-06-09 17:27:04 +0200

    Add State Matching

--- view/cobot.py
+++ view/cobot.py
@@ -1,5 +1,6 @@
 import requests
-from time import sleep
+import time
+from random import randint
 from pymycobot import MyCobot, Angle, Coord
 from pymycobot import MyCobotSocket
 
@@ -9,7 +10,7 @@ model_url = "http://localhost:1110/view/should_learn"
 
 class Cobot:
     def __init__(self, ip, port):
-        self.cobot = MyCobotSocket(ip, port);
+        self.cobot = MyCobotSocket(ip, port)
 
     def color_blink(self, rgb):
         # Should contain either red, blue or green set to 10
@@ -17,56 +18,54 @@ class Cobot:
         i = 0
         while i < 25:
             self.cobot.set_color(rgb[0] * i, rgb[1] * i, rgb[2] * i)
-            # sleep(0.005)
+            time.sleep(0.005)
             i += 2
 
     def straight(self, rgb):
         self.cobot.set_color(rgb[0], rgb[1], rgb[2])
         self.cobot.send_angles([0, 0, 0, 0, 0, 0], 20)
-        sleep(5)
+        time.sleep(5)
 
     def curled_up(self, rgb):
         self.cobot.set_color(rgb[0], rgb[1], rgb[2])
         self.cobot.send_angles([90, 140, -145, -60, -90, 0], 20)
-        sleep(5)
+        time.sleep(5)
 
     def curled_up_wiggle(self, rgb):
         # Default Setup for Curled state
         self.cobot.set_color(rgb[0], rgb[1], rgb[2])
         self.curled_up(rgb)
-        sleep(5)
+        time.sleep(5)
         self.cobot.send_angle(Angle.J4.value, 0, 20)
-        sleep(2)
+        time.sleep(2)
         # Start Animation
         self.cobot.send_angle(Angle.J4.value, 45, 40)
-        sleep(1)
+        time.sleep(1)
         self.cobot.send_angle(Angle.J4.value, -45, 40)
-        sleep(1)
+        time.sleep(1)
         self.cobot.send_angle(Angle.J4.value, 45, 40)
-        sleep(1)
+        time.sleep(1)
         self.cobot.send_angle(Angle.J4.value, -45, 40)
-        sleep(1)
+        time.sleep(1)
         self.cobot.send_angle(Angle.J4.value, 45, 40)
-        sleep(1)
+        time.sleep(1)
         self.cobot.send_angle(Angle.J4.value, 0, 40)
-        sleep(1)
+        time.sleep(1)
 
     def hide(self, rgb):
         self.cobot.set_color(rgb[0], rgb[1], rgb[2])
         self.cobot.send_angles([87.89, -93.6, -79.8, -98.17, -87.01, -91.4], 20)
-        sleep(10)
+        time.sleep(10)
 
     def lay_down(self, rgb):
         self.cobot.set_color(rgb[0], rgb[1], rgb[2])
         self.cobot.send_angles([90, 140, -40, -5, -90, 0], 20)
-        sleep(10)
+        time.sleep(10)
 
 
-# Print response every second
-def main():
+def preview():
     cobot = Cobot("10.42.0.141", 9000)
-
-    sleep(5)
+    time.sleep(5)
     print("Straight")
     cobot.straight([0, 0, 255])
     print("Color_Blink")
@@ -79,9 +78,65 @@ def main():
     cobot.lay_down([0, 0, 0])
     print("Straight")
     cobot.straight([255, 0, 0])
-    # while True:
-    #    print(requests.get(model_url).text)
-    #    sleep(1)
+
+
+def main():
+    # Hardcoded ==> minimum time between request checking
+    min_wait_between_calls = 30
+
+    cobot = Cobot("10.42.0.141", 9000)
+    # Intended State < 0 --> Same Action with Yellow Light
+    # Intended State >= 0 && <= 1.0 --> Intended State = Concentrated
+    # Intended State > 1.0 && < 2.0 --> Intended State = Pause
+    # Intended State == 2.0 --> Red Sraight
+    intended_state = 0.0
+    rgb = [0, 255, 0]
+
+    while True:
+        start = time.time()
+
+        rgb = [0, 255, 0]
+        # print(requests.get(model_url).text)
+        # TODO Fetch parameters from the model_url
+        # Change Color to Yellow if negative and change to normale action
+        if intended_state < 0:
+            rgb = [255, 255, 0]
+            intended_state = abs(intended_state)
+
+        # Call the respective cobot action for the given state ==>
+        # Color is already set (if not color_blink, straight, lay_down)
+        match intended_state:
+            case intended_state if 0 <= intended_state < 0.8:
+                cobot.lay_down([0, 0, 0])
+                print("lay_down(black)")
+            case intended_state if 0.8 <= intended_state < 1.0:
+                cobot.color_blink([0, 10, 0])
+                print("color_blink(green)")
+
+            case intended_state if 1.0 <= intended_state < 2.0:
+                # This should be the Pause Phase
+                choice = randint(0, 1)
+                match choice:
+                    case 0:
+                        cobot.curled_up_wiggle(rgb)
+                        print(intended_state)
+                    case 1:
+                        cobot.hide(rgb)
+                        print(intended_state)
+
+            case intended_state if intended_state == 2.0:
+                cobot.straight([255, 0, 0])
+                print("Straight(red)\n")
+                
+            case intended_state if (2.0 < intended_state or intended_state < -2.0):
+                break
+
+        end = time.time()
+        duration = round(end - start, 2)
+
+        # Wait until nim_wait has been reached and repeat action
+        if duration < min_wait_between_calls:
+            time.sleep(round(min_wait_between_calls - duration, 2))
 
 
 if __name__ == '__main__':
