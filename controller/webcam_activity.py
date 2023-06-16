e7eac6ba21c09189c99f081fdca394dceeaf31e9
Aaron Tacke <tacke@in.tum.de>
2023-06-16 10:51:21 +0200

    Check hand gestures for 3 secs

--- controller/webcam_activity.py
+++ controller/webcam_activity.py
@@ -7,11 +7,21 @@ leaving_url = "http://localhost:1110/model/is_leaving"
 back_url = "http://localhost:1110/model/is_back"
 
 
+# Check steps time whether the result of getter is the same
+def multiple_checks(getter, steps):
+    res = getter()
+    for _ in range(steps):
+        if getter() != res:
+            res = ""
+            break
+    return res
+
+
 # Call the model if a relevant hand gesture was detected
 def main():
     recognizer = HandGestureRecognizer()
     while True:
-        gesture = recognizer.get_current_gesture()
+        gesture = multiple_checks(recognizer.get_current_gesture, 20)
         if gesture == "stop" or gesture == "live long":
             requests.get(mad_url)
         if gesture == "thumbs up":
